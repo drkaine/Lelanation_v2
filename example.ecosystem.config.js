75cc0a0284ae29a0b6419a0b7de15eaaa3ad399b
drkaine <duretkevin@live.fr>
2026-01-20 11:03:58 +0000

    Add ecosystem.config.js for application management and update .gitignore
    - Introduced example.ecosystem.config.js for managing backend and frontend applications with PM2.
    - Updated .gitignore to exclude ecosystem.config.js from version control.

---
+++ example.ecosystem.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+    apps: [
+      {
+        name: 'lelanation-backend',
+        cwd: 'path',
+        script: 'npx',
+        args: 'tsx watch src/index.ts',
+        interpreter: 'none',
+        env: {
+          NODE_ENV: 'development',
+          PORT: 4001,
+          YOUTUBE_API_KEY: 'API-KEY'      
+        },
+        error_file: 'path',
+        out_file: 'path',
+        log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+        merge_logs: true,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '1G',
+        instances: 1,
+        exec_mode: 'fork'
+      },
+      {
+        name: 'lelanation-frontend',
+        cwd: 'path',
+        script: 'node',
+        args: '.output/server/index.mjs',
+        interpreter: 'none',
+        env: {
+          NODE_ENV: 'production',
+          PORT: 3000,
+          NITRO_PORT: 3000,
+          NITRO_HOST: '127.0.0.1'
+        },
+        error_file: 'path',
+        out_file: 'path',
+        log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+        merge_logs: true,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '1G',
+        instances: 1,
+        exec_mode: 'fork'
+      }
+    ]
+  }
+  
